06468c25fb72bb78db8987e1f83522a646911d5b
Ali Hassan <4li.h455an@gmail.com>
2023-07-27 00:59:22 +0530

    Updates

---
+++ precache-manifest.ef0316cd6ae617c16a9de06769085cc4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "406efbfbbd3fe42220ef65303c3adb34",
+    "url": "/alihassan28/RemindWrite/index.html"
+  },
+  {
+    "revision": "eb30d26c143ac17522e1",
+    "url": "/alihassan28/RemindWrite/static/js/2.d306e2ae.chunk.js"
+  },
+  {
+    "revision": "0cf5ba2c00bbc0b01089c885c528fd0c",
+    "url": "/alihassan28/RemindWrite/static/js/2.d306e2ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f95e5c4862ac44d3975",
+    "url": "/alihassan28/RemindWrite/static/js/main.68ba370c.chunk.js"
+  },
+  {
+    "revision": "359bce802a742225efae",
+    "url": "/alihassan28/RemindWrite/static/js/runtime-main.6ad0b3f7.js"
+  }
+]);
